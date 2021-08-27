d49045171a78b666bcb4b324deb08d30e38b63e2
Yurii Oleynikov <yurkao@gmail.com>
2021-08-27 15:30:36 +0300

    fixed tests: removed manual playground

--- openssl/tests/06-ssl-start-server.sh
+++ openssl/tests/06-ssl-start-server.sh
@@ -2,13 +2,8 @@
 
 # shellcheck disable=SC1090
 . "${TEST_DIR}/common"
-set -x
 for protocol in ${SSL_PROTOCOLS}; do
   port_var=$(test_port "${protocol}")
   start "Starting SSL/TLS server on ${port_var} with ${protocol}"
   openssl s_server -"${protocol}" -key "${CA_KEY}" -cert "${CA_CRT}" -accept "${port_var}" -www &
 done
-export PATH="/usr/local/bin:path"
-export protocol=ssl3
-export port_var=1444
-openssl s_server -"${protocol}" -engine gost -cipher aGOST -key "${CA_KEY}" -cert "${CA_CRT}" -accept "${port_var}" -www
