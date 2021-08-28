f98218fecdbfc39b918a199419fa8302e3052fd5
Yurii Oleynikov <yurkao@gmail.com>
2021-08-29 00:09:38 +0300

    simplified CA generation

--- openssl/tests/05-ssl-generate-ca.sh
+++ openssl/tests/05-ssl-generate-ca.sh
@@ -4,7 +4,6 @@
 . "${TEST_DIR}/common"
 
 start "Generating test CA certificate"
-# this is to generate ca w/o prompt
-# AB is for country code - should be of 2 symbols
-yes AB | openssl req -x509 -newkey rsa:2048 -keyout "${CA_KEY}" -out "${CA_CRT}" -days 365 -nodes > /dev/null
+SUBJECT="/C=XY/ST=XY/L=XY/O=XY/OU=root/CN=$(hostname -f)"
+openssl req -nodes -x509 -newkey rsa:2048 -keyout "${CA_KEY}" -out "${CA_CRT}" -subj "${SUBJECT}" > /dev/null 2>&1
 
